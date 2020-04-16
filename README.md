18eafde32c9dc25335299d23c4200b48043593a2
Yibo-Hao <sxyhao@hotmail.com>
2020-04-16 23:00:35 +0800

    换一种引入svg的办法，新加一个脚本命令

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # YiboUI
+
+[![Build Status](https://travis-ci.org/Yibo-Hao/YiboUI.svg?branch=master)](https://travis-ci.org/Yibo-Hao/YiboUI)
+## 介绍
+
+在学习 Vue 过程中做的一个 UI，希望对你有帮助，以后会模仿Element UI进一步开发
+
+## 开始使用
+
+1. 安装
+## 文档
+
+## 提问
+
+## 变更记录
+
+## 贡献代码
+
--- src/icon.vue
+++ src/icon.vue
@@ -1,38 +1,34 @@
 <template>
-    <svg v-if="icon in icons" class="icon">
-        <use :xlink:href="'#i-'+icon"></use>
-    </svg>
+  <svg v-if="icon in icons" class="icon">
+    <use :xlink:href="'#i-' + icon"></use>
+  </svg>
 </template>
 <script>
-    // import 'src/assets/icons/down.svg'
-    // import 'src/assets/icons/download.svg'
-    // import 'src/assets/icons/right.svg'
-    // import 'src/assets/icons/left.svg'
-    // import 'src/assets/icons/like.svg'
-    // import 'src/assets/icons/setting.svg'
 
-    export default {
-        data: function () {
-            return {
-                icons: {
-                    "down": "down",
-                    "setting":"setting",
-                    "download":"download",
-                    "right":"right",
-                    "left":"left",
-                    "like":"like",
-                    "load":"load"
-                }
-            }
-        },
-        props: ['icon']
-    }
+import "./svg";
+export default {
+  data: function() {
+    return {
+      icons: {
+        down: "down",
+        setting: "setting",
+        download: "download",
+        right: "right",
+        left: "left",
+        like: "like",
+        load: "load"
+      }
+    };
+  },
+  props: ["icon"]
+};
 </script>
 <style lang="scss" scoped>
-    .icon {
-        width: 1em; height: 1em;
-        vertical-align: -0.15em;
-        fill: currentColor;
-        overflow: hidden;
-    }
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
 </style>
