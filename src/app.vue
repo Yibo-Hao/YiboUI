01a2f83ab72e102ae3d0680d3593dbe644a0a5ab
Yibo-Hao <sxyhao@hotmail.com>
2020-04-24 01:48:52 +0800

    基本实现row 和 col 的对齐功能

--- src/app.vue
+++ src/app.vue
@@ -31,10 +31,10 @@
       <g-input v-model="message" error="名字不少于两个字"></g-input>
       <g-input v-model="message" :disabled="true"></g-input>
     </div>
-    <div class="grid" style="border: 1px solid red">
-      <g-row gutter="20">
-        <g-col span="2">1</g-col>
-        <g-col span="22">1</g-col>
+    <div class="grid">
+      <g-row gutter="20" align="center" justify="center">
+        <g-col span="10">1</g-col>
+        <g-col span="8">1</g-col>
       </g-row>
       <g-row gutter="20">
         <g-col>1</g-col>
--- src/grid/row.vue
+++ src/grid/row.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    class="row"
-    :style="{ marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px' }"
-  >
+  <div class="row" :style="rowStyle" :class="rowClass">
     <slot></slot>
   </div>
 </template>
@@ -13,12 +10,44 @@ export default {
   props: {
     gutter: {
       type: [Number, String]
+    },
+    align: {
+      type: String,
+      validator(value) {
+        return ["top", "center", "bottom"].includes(value);
+      }
+    },
+    justify: {
+      type: String,
+      validator(value) {
+        return [
+          "start",
+          "end",
+          "center",
+          "space-around",
+          "space-between"
+        ].includes(value);
+      }
     }
   },
   mounted() {
-    this.$children.forEach(vm=>{
-        vm.gutter = this.gutter
-    })
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter;
+    });
+  },
+  computed: {
+    rowStyle() {
+      let { gutter } = this;
+      return {
+        marginLeft: -gutter / 2 + "px",
+        marginRight: -gutter / 2 + "px"
+      };
+    },
+    rowClass() {
+      let { align } = this;
+      let { justify } = this;
+      return [align && `align-${align}`,justify && `justify-${justify}`];
+    }
   }
 };
 </script>
@@ -26,5 +55,31 @@ export default {
 <style lang="scss" scoped>
 .row {
   display: flex;
+  height: 100px;
+  border: 1px solid green;
+  &.align-center {
+    align-items: center;
+  }
+  &.align-top {
+    align-items: flex-start;
+  }
+  &.align-bottom {
+    align-items: flex-end;
+  }
+  &.justify-center {
+    justify-content: center;
+  }
+  &.justify-space-between {
+    justify-content: space-between;
+  }
+  &.justify-space-around {
+    justify-content: space-around;
+  }
+  &.justify-end {
+    justify-content: flex-end;
+  }
+  &.justify-start {
+    justify-content: flex-start;
+  }
 }
 </style>
