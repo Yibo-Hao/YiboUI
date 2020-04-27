954f1099150c873741756329cf87fbd21d5f306f
Yibo-Hao <sxyhao@hotmail.com>
2020-04-27 16:37:23 +0800

    实现 toast 不重复

--- src/toast/toastplugin.js
+++ src/toast/toastplugin.js
@@ -1,15 +1,24 @@
 import Toast from "./toast";
 
+function createToast({ Vue, message, toastoptions }) {
+  let Constructor = Vue.extend(Toast);
+  let toast = new Constructor({
+    propsData: toastoptions
+  });
+  toast.$slots.default = [message];
+  toast.$mount();
+  document.body.appendChild(toast.$el);
+  return toast
+}
+
+let currentToast;
 export default {
   install(Vue, options) {
-    Vue.prototype.$toast = function(message, toastoptions) {
-      let Constructor = Vue.extend(Toast);
-      let toast = new Constructor({
-        propsData: toastoptions
-      });
-      toast.$slots.default = [message];
-      toast.$mount();
-      document.body.appendChild(toast.$el);
+    Vue.prototype.$toast = function (message, toastoptions) {
+      if (currentToast){
+        currentToast.close()
+      }
+      currentToast = createToast({Vue,message,toastoptions})
     };
   }
 };
