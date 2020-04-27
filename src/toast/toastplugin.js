d93f4678dbad69a227d4afb91a9cc1aa52fbf981
Yibo-Hao <sxyhao@hotmail.com>
2020-04-27 17:15:12 +0800

    实现 toast close后增加一个回调

--- src/toast/toastplugin.js
+++ src/toast/toastplugin.js
@@ -1,24 +1,32 @@
 import Toast from "./toast";
 
-function createToast({ Vue, message, toastoptions }) {
+function createToast({ Vue, message, toastoptions, onClose }) {
   let Constructor = Vue.extend(Toast);
   let toast = new Constructor({
     propsData: toastoptions
   });
   toast.$slots.default = [message];
   toast.$mount();
+  toast.$on("beforeClose",onClose);
   document.body.appendChild(toast.$el);
-  return toast
+  return toast;
 }
 
 let currentToast;
 export default {
   install(Vue, options) {
-    Vue.prototype.$toast = function (message, toastoptions) {
-      if (currentToast){
-        currentToast.close()
+    Vue.prototype.$toast = function(message, toastoptions) {
+      if (currentToast) {
+        currentToast.close();
       }
-      currentToast = createToast({Vue,message,toastoptions})
+      currentToast = createToast({
+        Vue,
+        message,
+        toastoptions,
+        onClose: () => {
+          currentToast = null;
+        }
+      });
     };
   }
 };
