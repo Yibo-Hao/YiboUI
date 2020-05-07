765b3a6aeb85355f49dc40c897a2613756bbbf4b
Yibo-Hao <sxyhao@hotmail.com>
2020-05-07 19:49:37 +0800

    解决overflow hidden的bug

--- src/popover/popover.vue
+++ src/popover/popover.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="popover" @click.stop="pop">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div
+      class="content-wrapper"
+      v-if="visible"
+      @click.stop
+      ref="contentWrapper"
+    >
       <slot></slot>
     </div>
-    <slot name="reference"></slot>
+    <span ref="triggerWrapper">
+      <slot name="reference"></slot>
+    </span>
   </div>
 </template>
 <script>
@@ -16,7 +23,16 @@ export default {
     pop() {
       this.visible = !this.visible;
       if (this.visible === true) {
-        setTimeout(() => {
+        this.$nextTick(() => {
+          document.body.appendChild(this.$refs.contentWrapper);
+          let {
+            width,
+            height,
+            left,
+            top
+          } = this.$refs.triggerWrapper.getBoundingClientRect();
+          this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
+          this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
           const outClick = () => {
             this.visible = false;
             document.removeEventListener("click", outClick);
@@ -33,11 +49,10 @@ export default {
   display: inline-block;
   vertical-align: top;
   position: relative;
-  .content-wrapper {
-    position: absolute;
-    bottom: 100%;
-    left: 0;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-  }
+}
+.content-wrapper {
+  position: absolute;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  transform: translateY(-125%);
 }
 </style>
