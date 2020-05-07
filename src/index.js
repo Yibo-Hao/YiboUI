6c99ec293cbb8eff38656ad0ab53193ab2cc9c3a
Yibo-Hao <sxyhao@hotmail.com>
2020-05-07 14:08:18 +0800

    实现了popover的基本功能

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import Button from "./button";
 import Icon from "./icon";
 import ButtonGroup from "./button-group";
 import Input from "./input";
+import Popover from "./popover/popover";
 
 Vue.component("g-button", Button);
 Vue.component("g-icon", Icon);
 Vue.component("g-button-group", ButtonGroup);
 Vue.component("g-input", Input);
-
+Vue.component("g-popover", Popover);
 new Vue({
   el: "#app",
   render: h => h(App)
 });
-
